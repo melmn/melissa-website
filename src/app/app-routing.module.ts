3dad447ec60b110e66352a7361f55a15ac13d55b
Melissa <nosalme92@gmail.com>
2018-08-17 19:48:53 -0400

    set up router

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CalculatorComponent } from './calculator/calculator.component';
+
+const routes: Routes = [
+  { path: "calculator", component: CalculatorComponent },
+  { path: "", redirectTo: "/calculator", pathMatch: "full" }
+];
+
+@NgModule({
+  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
