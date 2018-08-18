a75a3c0c37318585576ba580221cade791686b03
Melissa <nosalme92@gmail.com>
2018-08-17 21:43:04 -0400

    making a calculator

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -6,10 +6,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
+  leftHandNumber: string;
+  rightHandNumber: string;
+  areWeTypingTheFirstNumber: boolean;
+  answer: string;
 
-  constructor() { }
+  constructor() {
+    this.leftHandNumber = "";
+    this.rightHandNumber = "";
+    this.areWeTypingTheFirstNumber = true;
+  }
 
   ngOnInit() {
   }
+
+
+  appendNumber(selectedNumber: string) {
+    if (this.areWeTypingTheFirstNumber === true) {
+      this.leftHandNumber = this.leftHandNumber + selectedNumber;
+    } else {
+      this.rightHandNumber = this.rightHandNumber + selectedNumber;
+    }
+
+    // console.log(this.leftHandNumber);
+  }
+
+  clear(): void {
+    this.leftHandNumber = "";
+  }
+
+  clearAll(): void {
+    this.leftHandNumber = "";
+    this.rightHandNumber = "";
+    this.answer = "";
+
+  }
+
+  backspace(): void {
+    this.leftHandNumber = this.leftHandNumber.slice(0, -1);
+  }
+
+  add() {
+    this.areWeTypingTheFirstNumber = false;
+  }
+
+  subtract() {
+  }
+
+  enter() {
+    let leftHandNumberValue: number = parseInt(this.leftHandNumber);
+    let rightHandNumberValue: number = +this.rightHandNumber;
+    let answer: number = leftHandNumberValue + rightHandNumberValue;
+    this.answer = answer.toString();
+    this.areWeTypingTheFirstNumber = true;
+    this.leftHandNumber = "";
+    this.rightHandNumber = "";
+  }
+
 
 }
